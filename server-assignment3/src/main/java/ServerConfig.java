3d8828c988b67c26ac37456fadd3bd75c64240ba
Teja Reddy Papadasu <tejareddypapadasu@gmail.com>
2025-11-29 15:53:00 -0800

    Delete server-assignment3/src/main/java directory

--- server-assignment3/src/main/java/ServerConfig.java
+++
@@ -1,7 +0,0 @@
-//package server;
-
-public class ServerConfig {
-    public static final String RMQ_HOST = "172.31.12.56"; // UPDATE THIS
-    public static final String EXCHANGE_NAME = "chat.exchange";
-    public static final int PORT = 8080;
-}
